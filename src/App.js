6cbbf3a2f26c0acd91d012c143d3b24bf9fd8199
Marcus <marcus-fischer@uiowa.edu>
2019-02-23 17:48:49 -0600

    Admin/Driver/Passenger Dashboards Iteration 2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Navigation from './components/Navigation/Navigation';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register'
 import Landing from './components/Landing/Landing';
-//import Admin from './components/Dashboard/Admin/Dashboard_Admin';
-//import Driver from './components/Dashboard/Driver/Dashboard_Driver';
-//import Pssanger from './components/Dashboard/Passenger/Dashboard_Passanger';
 import DriverCard from './components/Dashboard/Driver/DriverCard';
 import AdminCard from './components/Dashboard/Admin/AdminCard';
 import PassengerCard from './components/Dashboard/Passenger/PassengerCard';
