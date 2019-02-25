3a1e8e0e476a63bb641112b131824ef828af01d1
Karan Pahlani <pahlani@gmail.com>
2019-02-25 15:03:39 -0600

    master fill

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+import './App.css';
+
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input: '',
+      route: '',
+      isSignedIn: false
+
+    }
+
+  }
+
+
+
+    onRouteChange = (route) => {
+    if(route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home'){
+      this.setState({isSignedIn: true} )
+    }
+      this.setState({route: route})
+
+  }
+
+
+
+  render() {
+
+        let component = null;
+        switch(this.state.route) {
+          case 'signin':
+            component =
+                <div>
+                    <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
+                    <SignIn onRouteChange={this.onRouteChange}/>
+                </div>;
+            break;
+          case 'home':
+            component = <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />;
+            break;
+
+          case 'signup':
+            component =
+              <div>
+              <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
+              <Register onRouteChange={this.onRouteChange}/>
+              </div>;
+            break;
+
+          default:
+            component =
+                <div>
+                  <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
+                  <SignIn onRouteChange={this.onRouteChange} />
+                </div>
+                  ;
+        }
+
+
+
+    return (
+      <div className="App">
+        {component}
+      </div>
+    );
+  }
+}
+
+export default App;
