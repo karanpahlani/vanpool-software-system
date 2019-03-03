04f5ae703f1271c6846c42ae36d6ef7b819a7842
Marcus <marcus-fischer@uiowa.edu>
2019-03-03 17:04:55 -0600

    Rider Dashboard updated

--- src/components/Dashboard/Passenger/PassengerDashboard.js
+++ src/components/Dashboard/Passenger/PassengerDashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 
-const DriverCard = (props) => {
+const PassengerDashboard = (props) => {
     const {username} = props;
     return(
         <div className={" w-80 mw7 tc bg-light-gray dib br3 pa2 ma5 grow bw2 shadow-5"}>
@@ -14,7 +14,7 @@ const DriverCard = (props) => {
 
                         <article className="mw5 m3 center bg-white br3 pa2 ba b--black-20">
                             <div className="tc">
-                                <img src="http://tachyons.io/img/avatar_1.jpg" className="br-100 h4 w4 dib ba b--black-05 pa2" title="Photo of a kitty staring at you" />
+                                <img src="http://tachyons.io/img/avatar_1.jpg" className="mw4 br-100 h-100 w-100 dib ba b--black-10 pa2" title="Photo of a kitty staring at you" />
                                 <h1 className="f3 mb2">username</h1>
                                 <h2 className="f5 fw4 gray mt0">Rider</h2>
                             </div>
@@ -24,13 +24,13 @@ const DriverCard = (props) => {
                     <div className="fl w-33 pa2 tc">
 
                         <article className="center m3 mw5 br3  ba b--black-10 ">
-                            <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">Balance</h1>
-                            <div className="pa2 bt b--black-10">
+                            <h1 className="f4 bg-white br3 br--top black-60 mv0 pv2 ph3">Balance</h1>
+                            <div className=" bt b--black-10">
 
-                                <h1 className="f3 mb2">$10.45</h1>
+                                <h1 className="f3 mb3">$10.45</h1>
 
-                                <div className="bt b--black-10 pa3">
-                                 <a className="f4 w-90 link dim br3 ph3 pv2 mb1 dib white bg-gray" href="#0">Add Funds</a>
+                                <div className="bt b--black-10 pa0">
+                                    <a className="f4 w-90 link dim br3 ph3 pv2 mb2 mt2 dib white bg-gray" href="#0">Add Funds</a>
                                 </div>
 
                             </div>
@@ -38,18 +38,17 @@ const DriverCard = (props) => {
 
 
                     </div>
-                    <div className="fl w-33 pa2 tc">
+                    <div className="fl w-34 pa2 tc">
 
                         <article className="center m3 mw5 br3 hidden ba b--black-10 mv">
-                            <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">Actions</h1>
-                            <div className="pa2 bt b--black-10">
-                                <p className="f4 f5-ns lh-copy measure center">
+                            <h1 className="f4 bg-white br3 br--top black-60 mv0 pv2 ph3">Actions</h1>
+                            <div className="pa1 bt b--black-10">
+                                <p className="f4 lh-copy measure center">
 
                                     <a className="f4 w-90 link dim br3 ph3 pv1 mb1 dib white bg-gray" href="#0">Search for Ride</a>
-                                    <a className="f4 w-90 link dim br3 ph3 pv1 mb1 dib white bg-gray" href="#0">Payment Details</a>
+                                    <a className="f4 w-90 link dim br3 ph3 pv1 mb1 dib white bg-gray" href="#0">Payment Info</a>
                                     <a className="f4 w-90 link dim br3 ph3 pv1 mb1 dib white bg-gray" href="#0">Driving Data</a>
-                                    <a className="f4 w-90 link dim br3 ph3 pv1 mb1 dib white bg-gray" href="#0">Edit Account</a>
-                                    <a className="f4 w-90 link dim br3 ph3 pv1 mb1 dib white bg-gray" href="#0">Delete Account</a>
+                                    <a className="f4 w-90 link dim br3 ph3 pv1  dib white bg-gray" href="#0">Edit Account</a>
 
                                 </p>
                             </div>
@@ -59,6 +58,40 @@ const DriverCard = (props) => {
                     </div>
                 </article>
 
+                <article className="cf">
+                    <div className="fl pa2 w-100 tc">
+
+                        <article className="center m3 mw-100 br3  ba b--black-10 ">
+                            <h1 className="f4 bg-white br3 br--top black-60 mv0 pv2 ph3">Active Rides</h1>
+                            <div className=" bt b--black-10">
+
+
+                                <div className="b--black-10 pa2">Active Ride Here From DB</div>
+
+
+                            </div>
+                        </article>
+                    </div>
+
+                </article>
+
+                <article className="cf">
+                    <div className="fl pa2 w-100 tc">
+
+                        <article className="center m3 mw-100 br3  ba b--black-10 ">
+                            <h1 className="f4 bg-white br3 br--top black-60 mv0 pv2 ph3">Ride History</h1>
+                            <div className=" bt b--black-10">
+
+
+                                <div className="b--black-10 pa2">Ride History Listing From DB</div>
+                                <div className="b--black-10 pa2">Ride History Listing From DB</div>
+
+
+                            </div>
+                        </article>
+                    </div>
+
+                </article>
 
 
             </div>
@@ -66,4 +99,4 @@ const DriverCard = (props) => {
     );
 };
 
-export default DriverCard;
+export default PassengerDashboard;
