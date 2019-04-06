6365ddb32a604514fd12b92272e2dcd65dd8a6cb
Marcus Fischer <mfdomain@gmail.com>
2019-04-06 17:25:57 -0500

    Create route page implemented. Creates route name and inserts into vss api route table

--- src/components/CreateRoute/CreateRoute.js
+++ src/components/CreateRoute/CreateRoute.js
@@ -2,57 +2,88 @@ import React from 'react';
 
 
 
-const createRoute = (props) => {
-    return(
-        <div className={" w-80 mw7 tc bg-light-gray dib br3 pa2 ma5 mv6 bw2 shadow-5"}>
-            <div>
+class CreateRoute extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            route: ''
+        }
+    }
+
+    onCreateRouteChange = (event) => {
+        this.setState({route: event.target.value})
+    };
+
+    onSubmitCreateRoute = () => {
+        fetch('http://localhost:3000/createRoute', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                route: this.state.route,
+            })
+        })
+    }
 
 
-                <article className="cf">
-                    <div className="fl pa2 w-100 tc">
+    render() {
 
-                        <article className="w-90 m3 center bg-white br3 pa2 mv2 ba b--black-20">
-                            <div className="tc">
-                                <h1 className="f3 mb2">Create Route</h1>
 
-                            </div>
-                        </article>
+        return (
+            <div className={" w-80 mw7 tc bg-light-gray dib br3 pa2 ma5 mv6 bw2 shadow-5"}>
+                <div>
 
-                        <article className="w-90 m3 center bg-white br3 pa2 ba b--black-20">
-                            <div className="tl">
 
-                                <form>
+                        <div className="pa2 w-100 tc">
 
-                                    <div className=" pa2 w-50">
-                                        <label htmlFor="routeName" className="tl f7 db mb2">Route Name</label>
-                                        <input id="routeName" className="input-reset ba b--black-20 f3 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" placeholder="Enter a route name" />
+                            <article className="w-100 m3 center bg-white br3 mb1 pa2 ba b--black-20">
+                                <div className="tc">
+                                    <h1 className="f3 mb2">Create Route</h1>
+
+                                </div>
+                            </article>
+
+                            <article className=" w-100 m3 center bg-white br3 mb1 pa2 ba b--black-20">
+
+
+                                    <fieldset id="create_route" className="ba b--transparent ph0 mh0">
+
+                                        <div className="pa2 w-100">
+                                            <label htmlFor="route" className="tc tl f7 db mb2">Route Name</label>
+                                            <input
+                                                id="route"
+                                                className="center input-reset f3 pa2 db w-40"
+                                                type="route"
+                                                name="route"
+                                                aria-describedby="name-desc"
+                                                placeholder="Enter a route name"
+                                                onChange={this.onCreateRouteChange}
+                                            />
+                                        </div>
+
+                                    </fieldset>
+
+                                    <div className="pa2 w-100">
+                                        <input
+                                            onClick={this.onSubmitCreateRoute}
+                                            className="br3 white b ph3 pv2 input-reset ba b--black bg-gray pointer f6 dib"
+                                            type="submit" value="Create Route"
+
+                                        />
                                     </div>
 
-                                    <div className=" pl2 w-50">
-                                        <h4>Number of stops:
-                                        <select name="Number of Stops">
-                                            <option value="volvo">1</option>
-                                            <option value="saab">2</option>
-                                            <option value="fiat">3</option>
-                                            <option value="audi">4</option>
-                                        </select>
-                                        </h4>
-                                    </div>
-
-                                </form>
-
-                            </div>
-                        </article>
 
 
-                    </div>
-
-                </article>
+                            </article>
 
 
+                        </div>
+
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
+}
 
-export default createRoute;
+export default CreateRoute;
