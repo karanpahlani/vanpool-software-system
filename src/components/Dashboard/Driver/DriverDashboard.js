f5a80e86f57dd696d5e1edb3bd8fa85024f63471
Marcus Fischer <mfdomain@gmail.com>
2019-04-07 15:06:49 -0500

    Update DriverDashboard.js

--- src/components/Dashboard/Driver/DriverDashboard.js
+++ src/components/Dashboard/Driver/DriverDashboard.js
@@ -5,7 +5,7 @@ import React from 'react';
 const DriverDashboard = (props) => {
     return(
 
-        <div className={" w-80 mw7 tc bg-light-red dib br3 pa2 ma5 grow bw2 shadow-5"}>
+        <div className={" w-80 mw7 tc bg-light-gray dib br3 pa2 mv6 bw2 shadow-5"}>
 
             <div>
 
